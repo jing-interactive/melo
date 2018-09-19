13b0ef2ec549d347e5343011cd3601d578adabba
vinjn <vinjn.z@gmail.com>
2018-09-19 20:00:02 +0800

    Add WIP FlythroughCamera

--- src/MeshViewerApp.cpp
+++ src/MeshViewerApp.cpp
@@ -12,10 +12,87 @@
 #include "cigltf.h"
 #include "ciobj.h"
 
+#define FLYTHROUGH_CAMERA_IMPLEMENTATION
+#include "flythrough_camera/flythrough_camera.h"
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+struct FlythroughCamera : public CameraPersp
+{
+    double mElapsedSeconds;
+    ivec2 mMousePos, mPrevMousePos;
+
+    bool mIsKeyPressed[KeyEvent::KEY_LAST] = {false};
+    bool mIsRMousePressed = false;
+
+    vec3 pos = {3.0f, 3.0f, 3.0f};
+    vec3 look = {0.0f, 0.0f, 1.0f};
+    const vec3 up = {0.0f, 1.0f, 0.0f};
+    float degrees_per_cursor_move = 0.5f;
+    float max_pitch_rotation_degrees = 80.0f;
+    
+    bool activated = true;
+
+    void setup()
+    {
+        mElapsedSeconds = getElapsedSeconds();
+        //        mCam.lookAt(aabb.getMax() * 2.0f, aabb.getCenter());
+
+//        flythrough_camera_look_to(pos, look, up, glm::value_ptr(mViewMatrix), 0);
+
+        getWindow()->getSignalResize().connect([&] { setAspectRatio(getWindowAspectRatio()); });
+
+        getWindow()->getSignalKeyDown().connect(
+            [&](KeyEvent& event) { mIsKeyPressed[event.getCode()] = true; });
+
+        getWindow()->getSignalKeyUp().connect(
+            [&](KeyEvent& event) { mIsKeyPressed[event.getCode()] = false; });
+
+        getWindow()->getSignalMouseDown().connect(
+            [&](MouseEvent& ev) { mIsRMousePressed = ev.isRight(); });
+
+        getWindow()->getSignalMouseUp().connect([&](MouseEvent& ev) { mIsRMousePressed = false; });
+
+        getWindow()->getSignalMouseMove().connect([&](MouseEvent& ev) {
+            mIsRMousePressed = ev.isRight();
+            mMousePos = ev.getPos();
+        });
+
+        AppBase::get()->getSignalUpdate().connect([&] {
+            float delta_time_sec = getElapsedSeconds() - mElapsedSeconds;
+            mElapsedSeconds = getElapsedSeconds();
+
+            flythrough_camera_update(
+                glm::value_ptr(pos), glm::value_ptr(look), glm::value_ptr(up),
+                glm::value_ptr(mViewMatrix), delta_time_sec,
+                1.0f * (mIsKeyPressed[KeyEvent::KEY_LSHIFT] ? 2.0f : 1.0f) * activated,
+                degrees_per_cursor_move, max_pitch_rotation_degrees, mMousePos.x - mPrevMousePos.x,
+                mMousePos.y - mPrevMousePos.y, mIsKeyPressed[KeyEvent::KEY_w],
+                mIsKeyPressed[KeyEvent::KEY_a], mIsKeyPressed[KeyEvent::KEY_s],
+                mIsKeyPressed[KeyEvent::KEY_d], mIsKeyPressed[KeyEvent::KEY_SPACE],
+                mIsKeyPressed[KeyEvent::KEY_LCTRL], 0);
+
+            if (activated)
+            {
+                float* view = glm::value_ptr(mViewMatrix);
+                printf("\n");
+                printf("pos: %f, %f, %f\n", pos[0], pos[1], pos[2]);
+                printf("look: %f, %f, %f\n", look[0], look[1], look[2]);
+                printf("view: %f %f %f %f\n"
+                       "      %f %f %f %f\n"
+                       "      %f %f %f %f\n"
+                       "      %f %f %f %f\n",
+                       view[0], view[1], view[2], view[3], view[4], view[5], view[6], view[7],
+                       view[8], view[9], view[10], view[11], view[12], view[13], view[14],
+                       view[15]);
+                mModelViewCached = true;
+            }
+            mPrevMousePos = mMousePos;
+        });
+    }
+};
 struct MeshViewerApp : public App
 {
     CameraPersp mCam;
@@ -29,6 +106,8 @@ struct MeshViewerApp : public App
 
     RootGLTFRef mRootGLTF;
     RootObjRef mRootObjRef;
+
+    FlythroughCamera mFlyCam;
 
     vector<string> listGlTFFiles()
     {
@@ -60,6 +139,8 @@ struct MeshViewerApp : public App
     void setup() override
     {
         log::makeLogger<log::LoggerFile>();
+
+        mFlyCam.setup();
 
         auto skyBoxShader = am::glslProg("SkyBox.vert", "SkyBox.frag");
         skyBoxShader->uniform("uCubeMapTex", 0);
@@ -123,7 +204,11 @@ struct MeshViewerApp : public App
         });
 
         getWindow()->getSignalDraw().connect([&] {
+#if 0
             gl::setMatrices(mCam);
+#else
+            gl::setMatrices(mFlyCam);
+#endif
             gl::clear();
 
             if (XYZ_VISIBLE)
