79ff834f82bdf601ba8e15f1f0afcb4b00411b49
vinjn <vinjn.z@gmail.com>
2019-02-08 22:15:00 +0800

    bat

--- samples/MeshViewer/build-vs2015.bat
+++ samples/MeshViewer/build-vs2015.bat
@@ -1,5 +1,6 @@
 cd %~dp0\vc2015
-msbuild %proj% /p:Configuration=Debug /p:Platform=x64 /v:minimal /m
+REM msbuild %proj% /p:Configuration=Debug /p:Platform=x64 /v:minimal /m
 msbuild %proj% /p:Configuration=Release /p:Platform=x64 /v:minimal /m
 REM msbuild %proj% /p:Configuration=Debug_Shared /p:Platform=x64 /v:minimal /m
 REM msbuild %proj% /p:Configuration=Release_Shared /p:Platform=x64 /v:minimal /m
+cd ..
